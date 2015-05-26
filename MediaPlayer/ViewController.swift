4bdd96b59ab366231f574ec7a49eccccf82522e6
crystalqujingjing <jingjingqu@jingjings-MacBook-Pro.local>
2015-05-25 19:41:07 -0700

    Initial Commit

---
+++ MediaPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaPlayer
+//
+//  Created by jingjing qu on 5/25/15.
+//  Copyright (c) 2015 TYX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
